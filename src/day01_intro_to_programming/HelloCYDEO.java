c8c4311ac0f4f5936e4c09fbb6d1967531f75dcf
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-02-22 16:34:43 -0800

    day01_IntroToProgramming

---
+++ src/day01_intro_to_programming/HelloCYDEO.java
@@ -0,0 +1,18 @@
+package day01_intro_to_programming;
+
+public class HelloCYDEO {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello CYDEO");
+        System.out.println();
+        System.out.println("Welcome to Java Developer Programming");
+        System.out.println();
+        System.out.println("I love Java");
+
+
+
+    }
+
+}
