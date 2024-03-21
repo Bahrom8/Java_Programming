ab7e3ba55d204a9742c41f7d0961328346810489
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-03-21 12:54:16 -0700

    Utilities

---
+++ src/utilities/Test2.java
@@ -0,0 +1,26 @@
+package utilities;
+
+import day27_accessModifiers.Data;
+
+import static day27_accessModifiers.Data.d;
+import static day27_accessModifiers.Data.method3;
+import static day27_accessModifiers.Data.*;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(d);
+        method3();
+        method4();
+
+        System.out.println(f);
+        System.out.println(e);
+
+       // System.out.println(a);
+
+
+    }
+
+
+}
